1e79cdfe40bcf9a0607f8f953b01a1cbba1745db
Yanis540 <yanistabellout4@gmail.com>
2024-10-13 21:54:27 +0200

    [Features] : added dropdown for user

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { ThemeToggle } from './theme-provider';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from './ui/button';
+import Account from './Account';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface NavbarProps {
@@ -16,7 +18,13 @@ function Navbar({}:NavbarProps) {
             <Link href="/" className="relative h-16 w-16">
                 <Image src="/assets/logo.png" fill className="w-full h-full" alt="logo" /> 
             </Link>
-            <div className="flex-1 flex flex-row items-center justify-end">
+            <div className="flex-1 flex flex-row gap-x-2 md:gap-x-4  items-center justify-end">
+                <div className="flex flex-row gap-x-2 md:gap-x-4 items-center">
+                    <Link href="/auth/sign-in">
+                        <Button  className='text-background bg-primary hover:bg-background hover:text-primary border-[1px] border-primary'>Login</Button>
+                    </Link>
+                    <Account /> 
+                </div>
                 <ThemeToggle /> 
             </div>
         </header>
