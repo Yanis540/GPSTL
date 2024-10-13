fb3f998f24bf84d2ae93b02717f049003d4a441a
Yanis540 <yanistabellout4@gmail.com>
2024-10-13 15:50:44 +0200

    [GPSTL-1] : modified navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { ThemeToggle } from './theme-provider';
+import Image from 'next/image';
+import Link from 'next/link';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface NavbarProps {
@@ -11,7 +13,9 @@ function Navbar({}:NavbarProps) {
     return (
     <div className=" sticky z-50 top-0  inset-x-0 h-16 ">
         <header className={cn('relative flex flex-row items-center h-full px-2 sm:px-16 md:px-8  transition-all duration-500 border-b-[1px] dark:border-primary py-2 mx-auto max-w-[1500px]',)}>
-            <h2 className="text-primary dark:text-foreground text-xl md:text-2xl font-bold">Alternicielles</h2>
+            <Link href="/" className="relative h-16 w-16">
+                <Image src="/assets/logo.png" fill className="w-full h-full" alt="logo" /> 
+            </Link>
             <div className="flex-1 flex flex-row items-center justify-end">
                 <ThemeToggle /> 
             </div>
