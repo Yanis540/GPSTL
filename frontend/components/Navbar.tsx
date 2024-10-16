e8ae43f914d2f02fc10d1a5af9a08c3ab89bda26
Yanis540 <yanistabellout4@gmail.com>
2024-10-16 10:59:12 +0200

    [GPSTL-16] : small ui fix

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -18,12 +18,12 @@ function Navbar({ }: NavbarProps) {
   
     return (
         <div className=" sticky z-50 top-0  inset-x-0 h-16 bg-background ">
-            <header className={cn('relative flex flex-row items-center h-full px-2 sm:px-16 md:px-8  transition-all duration-500 border-b-[1px] dark:border-primary py-2 mx-auto max-w-[1500px]',)}>
+            <header className={cn('relative flex flex-row items-center justify-between  h-full px-2 sm:px-16 md:px-8  transition-all duration-500 border-b-[1px] dark:border-primary py-2 mx-auto max-w-[1500px]',)}>
                 <Link href="/" className="relative h-16 w-16">
                     <Image src="/assets/logo.png" fill className="w-full h-full" alt="logo" />
                 </Link>
                 <Navigation /> 
-                <div className="flex-1 md:flex-0 flex flex-row gap-x-2 md:gap-x-4  items-center justify-end">
+                <div className=" flex flex-row gap-x-2 md:gap-x-4  items-center justify-end">
                     <div className="flex flex-row gap-x-2 md:gap-x-4 items-center ">
                         <NavigationMobile /> 
                         {
